93246b3f7f1d27cae4a6cf0b92581fbda6a345ab
toshi <tab.hsts@gmail.com>
2021-07-22 02:57:35 +0900

    パネルのレイアウト実装

--- PanelGame/ContentView.swift
+++ PanelGame/ContentView.swift
@@ -11,23 +11,22 @@ struct ContentView: View {
     //  パネルの縦横の長さ
     let gridLength = (UIScreen.main.bounds.size.width - 60) / 3
     //  パネルの数、レイアウト設定
-    var columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: -15), count: 3)
+    var columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 15), count: 3)
     
     var body: some View {
-        ZStack {
-            VStack{
-                LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
-                    ForEach((0...8), id: \.self) { num in
-                        ZStack {
-                            // 角丸の四角形を描画する
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.blue)
-                                .frame(width: gridLength, height: gridLength)
-                        }
+        VStack{
+            LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
+                ForEach((0...8), id: \.self) { num in
+                    ZStack {
+                        // 角丸の四角形を描画する
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.blue)
+                            .frame(height: gridLength)
                     }
                 }
             }
         }
+        .padding(.horizontal, 15)
     }
 }
 
