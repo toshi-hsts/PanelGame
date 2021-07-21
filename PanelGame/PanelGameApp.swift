e2d58c1e62c56da97875e845b89d57fc32722240
toshi <tab.hsts@gmail.com>
2021-07-21 14:55:56 +0900

    Initial Commit

---
+++ PanelGame/PanelGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanelGameApp.swift
+//  PanelGame
+//
+//  Created by user1 on 2021/07/21.
+//
+
+import SwiftUI
+
+@main
+struct PanelGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
