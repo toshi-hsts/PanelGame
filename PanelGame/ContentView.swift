b17070fbd24d12020a25aaf3b8bc031f2ac27b17
toshi <tab.hsts@gmail.com>
2021-07-23 16:30:10 +0900

    パネルにプレイヤーを配置できるようにする

--- PanelGame/ContentView.swift
+++ PanelGame/ContentView.swift
@@ -12,16 +12,39 @@ struct ContentView: View {
     let gridLength = (UIScreen.main.bounds.size.width - 60) / 3
     //  パネルの数、レイアウト設定
     var columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 15), count: 3)
+    // パネルの状態を配列で管理
+    @State var panels = Array(repeating: "", count: 9)
+    // 二人のプレイヤーを切り替えるためのBool値
+    @State var playerSwitcher = true
     
     var body: some View {
         NavigationView{
             VStack{
                 LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
-                    ForEach((0...8), id: \.self) { num in
+                    ForEach((0...8), id: \.self) { panelNumber in
                         ZStack {
-                            // 角丸の四角形を描画する
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(height: gridLength)
+                            // パネルが空白のときの処理
+                            if panels[panelNumber].isEmpty {
+                                // 角丸の四角形を描画する
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(height: gridLength)
+                                    // tapしたときの挙動
+                                    .onTapGesture {
+                                        // パネルのプレイヤーを格納
+                                        panels[panelNumber] = playerSwitcher ? "🐶":"😸"
+                                        // プレイヤーを切り替える
+                                        playerSwitcher.toggle()
+                                    }
+                                // パネルが埋まっているときの処理
+                            } else {
+                                // パネルをオレンジにする
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.orange)
+                                    .frame(height: gridLength)
+                                // パネルの上にプレイヤーを表示する
+                                Text(panels[panelNumber])
+                                    .font(.system(size: gridLength / 2))
+                            }
                         }
                     }
                 }
