c4b7b60ccd4056ae9a81641dd3f07f279063ead9
toshi <tab.hsts@gmail.com>
2021-08-24 16:41:22 +0900

    タイトルの表示方法を変更 #9

--- PanelGame/View/HomeView.swift
+++ PanelGame/View/HomeView.swift
@@ -16,76 +16,77 @@ struct HomeView: View {
     @StateObject private var homeViewModel = HomeViewModel()
     
     var body: some View {
-        NavigationView{
-            VStack{
-                HStack{
-                    // おじいちゃんView
-                    PlayerImageView(homeViewModel: homeViewModel, gridLength: gridLength, player: .grandPa)
-                    // VSテキストを表示
-                    Text("VS")
-                        .font(.largeTitle)
-                        .padding(.horizontal,10)
-                    // おばあちゃんView
-                    PlayerImageView(homeViewModel: homeViewModel, gridLength: gridLength, player: .grandMa)
-                }
-                // 先手をテキストで表示する
-                Text(homeViewModel.firstPlayer.firstTurnMessage)
-                
-                LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
-                    ForEach((0...15), id: \.self) { panelNumber in
-                        ZStack {
-                            // 角丸の四角形を描画する
-                            RoundedRectangle(cornerRadius: 10)
-                                // パネルの色
-                                .changePanelColor(homeViewModel.panels[panelNumber])
-                                // gridの高さ
-                                .frame(height: gridLength)
-                            // パネルに割り当てられたプレイヤーがいる場合、パネルの上に画像を表示する
-                            if homeViewModel.panels[panelNumber] != .none {
-                                Image(homeViewModel.panels[panelNumber].toString())
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: gridLength * 0.7, height: gridLength * 0.7, alignment: .center)
-                            }
-                        }
-                        // tapしたときの挙動
-                        .onTapGesture {
-                            // 選択済みのパネルの場合は、何もしない
-                            guard homeViewModel.panels[panelNumber] == .none else {
-                                return
-                            }
-                            // 最初にパネルがタップされたときにゲームを開始する
-                            if homeViewModel.isStartingGame == false {
-                                // ゲーム開始時の現在のプレイヤーを先手プレイヤーとする
-                                homeViewModel.firstPlayer = homeViewModel.currentPlayer
-                                // ゲームを開始中にする
-                                homeViewModel.isStartingGame = true
-                            }
-                            // アニメーションを利用する
-                            withAnimation(){
-                                // パネルのプレイヤーを格納
-                                homeViewModel.panels[panelNumber] = homeViewModel.currentPlayer
-                            }
-                            // 勝敗を判定する
-                            homeViewModel.judgeGame(player: homeViewModel.panels[panelNumber])
-                        }
-                        // パネルをめくるアニメーション設定
-                        .rotation3DEffect(
-                            Angle.degrees(homeViewModel.panels[panelNumber] == .none ? 0 : 180),
-                            axis: (x:0, y:1, z:0),
-                            anchor: .center,
-                            perspective: 1)
-                    }
-                }
-                // 15ポイントの余白を水平方向に付与
+        VStack{
+            Text("四目並べ")
+                .font(.largeTitle)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.horizontal, 15)
-                // どちらのターンかを知らせるメッセージ
-                Text(homeViewModel.currentPlayer.whoseTurnMessage)
-                    .padding()
-                    .font(.title)
+            HStack{
+                // おじいちゃんView
+                PlayerImageView(homeViewModel: homeViewModel, gridLength: gridLength, player: .grandPa)
+                // VSテキストを表示
+                Text("VS")
+                    .font(.largeTitle)
+                    .padding(.horizontal,10)
+                // おばあちゃんView
+                PlayerImageView(homeViewModel: homeViewModel, gridLength: gridLength, player: .grandMa)
             }
-            // ナビゲーションバータイトル
-            .navigationTitle("四目並べ")
+            // 先手をテキストで表示する
+            Text(homeViewModel.firstPlayer.firstTurnMessage)
+            
+            LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
+                ForEach((0...15), id: \.self) { panelNumber in
+                    ZStack {
+                        // 角丸の四角形を描画する
+                        RoundedRectangle(cornerRadius: 10)
+                            // パネルの色
+                            .changePanelColor(homeViewModel.panels[panelNumber])
+                            // gridの高さ
+                            .frame(height: gridLength)
+                        // パネルに割り当てられたプレイヤーがいる場合、パネルの上に画像を表示する
+                        if homeViewModel.panels[panelNumber] != .none {
+                            Image(homeViewModel.panels[panelNumber].toString())
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: gridLength * 0.7, height: gridLength * 0.7, alignment: .center)
+                        }
+                    }
+                    // tapしたときの挙動
+                    .onTapGesture {
+                        // 選択済みのパネルの場合は、何もしない
+                        guard homeViewModel.panels[panelNumber] == .none else {
+                            return
+                        }
+                        // 最初にパネルがタップされたときにゲームを開始する
+                        if homeViewModel.isStartingGame == false {
+                            // ゲーム開始時の現在のプレイヤーを先手プレイヤーとする
+                            homeViewModel.firstPlayer = homeViewModel.currentPlayer
+                            // ゲームを開始中にする
+                            homeViewModel.isStartingGame = true
+                        }
+                        // アニメーションを利用する
+                        withAnimation(){
+                            // パネルのプレイヤーを格納
+                            homeViewModel.panels[panelNumber] = homeViewModel.currentPlayer
+                        }
+                        // 勝敗を判定する
+                        homeViewModel.judgeGame(player: homeViewModel.panels[panelNumber])
+                    }
+                    // パネルをめくるアニメーション設定
+                    .rotation3DEffect(
+                        Angle.degrees(homeViewModel.panels[panelNumber] == .none ? 0 : 180),
+                        axis: (x:0, y:1, z:0),
+                        anchor: .center,
+                        perspective: 1)
+                }
+            }
+            // 15ポイントの余白を水平方向に付与
+            .padding(.horizontal, 15)
+            // どちらのターンかを知らせるメッセージ
+            Text(homeViewModel.currentPlayer.whoseTurnMessage)
+                .padding()
+                .font(.title2)
         }
         // ダークモード に強制する
         .preferredColorScheme(.dark)
