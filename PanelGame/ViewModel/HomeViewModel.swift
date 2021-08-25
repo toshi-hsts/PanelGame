bcc1a807687361d3037c354b3375413d0397eef5
toshi <tab.hsts@gmail.com>
2021-08-25 20:36:27 +0900

    パネル数を定数化　#11

--- PanelGame/ViewModel/HomeViewModel.swift
+++ PanelGame/ViewModel/HomeViewModel.swift
@@ -8,16 +8,23 @@
 import Foundation
 
 class HomeViewModel: ObservableObject {
+    // パネル数
+    private let NUMBER_OF_PANELS = 16
     // パネルの状態を配列で管理
-    @Published var panels: [PlayerModel] = Array(repeating: PlayerModel.none, count: 16)
+    @Published var panels: [PlayerModel]!
     // 現在の差し手を管理
-    @Published var currentPlayer = PlayerModel.grandPa
+    @Published var currentPlayer: PlayerModel!
     // 先手プレイヤー管理
-    @Published var firstPlayer = PlayerModel.none
-    // アラートを表示するか管理
-    @Published var showAlert = false
+    @Published var firstPlayer: PlayerModel!
     // ゲーム中かを管理
-    @Published var isStartingGame = false
+    @Published var isStartingGame: Bool!
+    // アラートを表示するか管理
+    @Published var showAlert: Bool!
+    
+    // 初期化処理
+    init() {
+        gameSet()
+    }
     
     // 勝敗を判定する
     func judgeGame(player: PlayerModel){
@@ -27,7 +34,7 @@ class HomeViewModel: ObservableObject {
         }
         // 引き分け時の処理
         else if panels.contains(PlayerModel.none) == false {
-            currentPlayer = .none
+            currentPlayer = PlayerModel.none
             showAlert = true
             // 引き分けでも勝利でもない場合の処理
         } else{
@@ -43,8 +50,8 @@ class HomeViewModel: ObservableObject {
         
         // 横方向で図柄が揃ったかチェックする
         if canWin == false {
-            for i in stride(from: 0, through: 12, by: 4){
-                for j in stride(from: i, through: i + 3, by: 1){
+            for i in stride(from: 0, to: NUMBER_OF_PANELS, by: 4){
+                for j in stride(from: i, to: i + 4, by: 1){
                     guard player == panels[j] else{
                         canWin = false
                         break
@@ -56,8 +63,8 @@ class HomeViewModel: ObservableObject {
         }
         // 縦方向で図柄が揃ったかチェックする
         if canWin == false {
-            for i in stride(from: 0, through: 3, by: 1){
-                for j in stride(from: i, through: i + 12, by: 4){
+            for i in stride(from: 0, to: 4, by: 1){
+                for j in stride(from: i, to: NUMBER_OF_PANELS, by: 4){
                     guard player == panels[j] else{
                         canWin = false
                         break
@@ -69,7 +76,7 @@ class HomeViewModel: ObservableObject {
         }
         // 左上から右下に図柄が揃ったかチェックする
         if canWin == false {
-            for i in stride(from: 0, through: 15, by: 5){
+            for i in stride(from: 0, to: NUMBER_OF_PANELS, by: 5){
                 guard player == panels[i] else{
                     canWin = false
                     break
@@ -93,9 +100,10 @@ class HomeViewModel: ObservableObject {
     
     // gameを初期状態に戻す
     func gameSet(){
-        panels = Array(repeating: PlayerModel.none, count: 16)
-        firstPlayer = .none
-        currentPlayer = .grandPa
+        panels = Array(repeating: PlayerModel.none, count: NUMBER_OF_PANELS)
+        firstPlayer = PlayerModel.none
+        currentPlayer = PlayerModel.grandPa
         isStartingGame = false
+        showAlert = false
     }
 }
